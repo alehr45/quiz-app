aae7dab2c9ed469094412459d59c399939c56a41
Your NameAlex <alehr45@gmail.com>
2021-01-13 08:57:57 -0600

    starting on js file

---
+++ assets/js/quiz.js
@@ -0,0 +1,11 @@
+
+
+//Variables and arrays for storing and gathering user information
+var question = document.getElementById("question");
+var choices = Array.from(document.getElementsByClassName("choice-text"))
+var codeQuestion = {};
+var codeAnswers = true;
+var score = 0;
+var questionData = 0;
+var allQuestions = [];
+
